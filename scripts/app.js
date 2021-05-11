a9f35eecc22e25075325eacb9925d95c2cecded6
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-11 12:15:09 -0400

    start button working

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
-console.log($)
+console.log('Welcome to Poke-A-Square')
+
+// CODE ORGANIZATION
+
+// Third Party Libraries: moment, underscore, jquery
+// Constants
+// App State
+// Cached DOM Elements
+// Event Listeners
+// Functions
+// steps to start game
+    // select button
+    // save button in variable
+    // add click event listener to button
+
+// -------------------- App State
+
+const squareCount = 25;
+
+// -------------------- Cached DOM elements
+
+const startButton = document.getElementById('startGame');
+
+//  ------------------- Event Listeners
+
+startButton.addEventListener('click', handleStartGame);
+
+// -------------------- Function Declarations
+
+function handleStartGame() {
+    console.log('Start Game clicked...!');
+}

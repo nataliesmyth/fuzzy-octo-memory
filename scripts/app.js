7b966a6f3ae4da1d60bcb1000b369d2bdae6155f
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-11 22:37:04 -0400

    thirty second timer

--- scripts/app.js
+++ scripts/app.js
@@ -16,10 +16,12 @@ console.log('Welcome to Poke-A-Square')
 // -------------------- App State
 
 const squareCount = 25;
+let time = 30;
 
 // -------------------- Cached DOM elements
 
 const startButton = document.getElementById('startGame');
+console.log(startButton)
 
 //  ------------------- Event Listeners
 
@@ -27,6 +29,24 @@ startButton.addEventListener('click', handleStartGame);
 
 // -------------------- Function Declarations
 
+// when function runs, the timer will start and the divs will fill with colors
 function handleStartGame() {
     console.log('Start Game clicked...!');
+    // Start Timer
+    startTimer()
+}
+// grab time and save it as a timer
+// pull timer out of start game for readability
+
+function startTimer() {
+
+    const timer = setInterval(function () {
+        if (time > 0) {
+            time--;
+            console.log(time);
+        } else {
+            console.log('Time is up')
+            clearInterval(timer);
+        }
+    }, 1000)
 }

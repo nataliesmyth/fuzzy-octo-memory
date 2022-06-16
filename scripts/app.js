b8d538a7a9f029d3731977a0abc9dede608ce279
Nataliesmyth <nataliesmyth11@gmail.com>
2022-06-16 01:36:42 -0400

    beginning a modal for user to add their name

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,21 @@
 // console.log('Welcome to Poke-A-Square')
 
+// -------------------- Modal
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+
 // -------------------- App State
 
 let time = 30;
